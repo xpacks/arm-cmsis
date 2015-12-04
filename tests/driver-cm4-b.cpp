fe81063d55a78f175f356b6c1df1047b5b0b2ce5
Liviu Ionescu <ilg@livius.net>
2015-12-04 20:33:18 +0200

    tests: add drivers.h  to include all driver headers

--- tests/driver-cm4-b.cpp
+++ tests/driver-cm4-b.cpp
@@ -7,20 +7,7 @@
  * Compile test for the CMSIS Driver headers.
  */
 
-#include "Driver_CAN.h"
-#include "Driver_ETH_MAC.h"
-#include "Driver_ETH_PHY.h"
-#include "Driver_ETH.h"
-#include "Driver_Flash.h"
-#include "Driver_I2C.h"
-#include "Driver_MCI.h"
-#include "Driver_NAND.h"
-#include "Driver_SAI.h"
-#include "Driver_SPI.h"
-#include "Driver_USART.h"
-#include "Driver_USB.h"
-#include "Driver_USBD.h"
-#include "Driver_USBH.h"
+#include "drivers.h"
 
 int
 main (int argc __attribute__((unused)), char* argv[] __attribute__((unused)))
---
+++ tests/drivers.h
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2015 Liviu Ionescu.
+// This file is part of the xPacks project (https://xpacks.github.io).
+//
+
+#include "Driver_CAN.h"
+#include "Driver_ETH_MAC.h"
+#include "Driver_ETH_PHY.h"
+#include "Driver_ETH.h"
+#include "Driver_Flash.h"
+#include "Driver_I2C.h"
+#include "Driver_MCI.h"
+#include "Driver_NAND.h"
+#include "Driver_SAI.h"
+#include "Driver_SPI.h"
+#include "Driver_USART.h"
+#include "Driver_USB.h"
+#include "Driver_USBD.h"
+#include "Driver_USBH.h"
