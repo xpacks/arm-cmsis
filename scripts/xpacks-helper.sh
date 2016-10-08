e4ae8afcf6d76f8fc355ce2dd2826ca4c6107c6e
Liviu Ionescu <ilg@livius.net>
2016-10-08 22:52:35 +0300

    xpacks-helper: change to subfolder/{include|src}

--- scripts/xpacks-helper.sh
+++ scripts/xpacks-helper.sh
@@ -14,14 +14,14 @@ do_add_arm_cmsis_xpack() {
   do_select_pack_folder "ilg/${pack_name}.git"
 
   # Always add 'core'.
-  do_prepare_dest "${pack_name}/include/core"
+  do_prepare_dest "${pack_name}/core/include"
   do_add_content "${pack_folder}/CMSIS/Include"/*
 
   while [ $# -ge 1 ]
   do
     case $1 in
     driver)
-      do_prepare_dest "${pack_name}/include/driver"
+      do_prepare_dest "${pack_name}/driver/include"
       do_add_content "${pack_folder}/CMSIS/Driver/Include"/*
       ;;
     esac
