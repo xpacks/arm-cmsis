3cf0fe832f86b8cf4ff69129aee53a6461ec15c9
Liviu Ionescu <ilg@livius.net>
2016-10-08 12:22:43 +0300

    add xpacks-helper.sh

---
+++ scripts/xpacks-helper.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#set -euo pipefail
+#IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Bash helper script used in project generate.sh scripts.
+# -----------------------------------------------------------------------------
+
+# Optional arguments: 'driver'.
+do_add_arm_cmsis_xpack() {
+  local pack_name='arm-cmsis'
+  do_tell_xpack "${pack_name}-xpack"
+
+  do_select_pack_folder "ilg/${pack_name}.git"
+  do_check_xpacks "${pack_name}"
+
+  # Always add 'core'.
+  do_prepare_dest "${pack_name}/include/core"
+  do_add_content "${pack_folder}/CMSIS/Include"/*
+
+  while [ $# -ge 1 ]
+  do
+    case $1 in
+    driver)
+      do_prepare_dest "${pack_name}/include/driver"
+      do_add_content "${pack_folder}/CMSIS/Driver/Include"/*
+      ;;
+    esac
+    shift
+  done
+}
+
+# -----------------------------------------------------------------------------
